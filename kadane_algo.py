6071b70ff6faff78dc148176799a14b115bd5d39
shanugoyalji <shanu.goyal.ji@gmail.com>
2026-02-12 18:41:53 +0530

    Create kadane_algo.py

---
+++ kadane_algo.py
@@ -0,0 +1,14 @@
+# Function to find the sum of contiguous subarray with maximum sum.
+
+class Solution:
+
+    def maxSubarraySum(self, arr):
+        # Initialize variables
+        max_so_far = arr[0]
+        current_max = arr[0]
+        
+        for i in range(1, len(arr)):
+            current_max = max(arr[i], current_max + arr[i])
+            max_so_far = max(max_so_far, current_max)
+            
+        return max_so_far
